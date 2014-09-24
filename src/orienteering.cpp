88b609e5769875c36fe80ca01acb09340c052f75
詹昊恂 <programmer.zhx@gmail.com>
2014-09-24 13:11:23 +0800

    Implemented loading method.

--- src/orienteering.cpp
+++ src/orienteering.cpp
@@ -16,15 +16,29 @@
 
 #include <iostream>
 #include <vector>
+#include <utility>
 
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::pair;
+using std::make_pair;
+using std::string;
 
 // Two-dimension matrix.
 using DistanceMatrix = vector<vector<int>>;
+// Coordinate of symbols. Given that "Coordinate example", "example.first"
+// represents the row index and "example.second" represents the column index.
+using Coordinate = pair<int, int>;
+
+
+const char kStartSymbol = 'S';
+const char kGoalSymbol = 'G';
+const char kCheckpointSymbol = '@';
+const char kOpenBlockSymbol = '.';
+const char kCloseBlockSymbol = '#';
 
 
 // Strategy:
@@ -34,7 +48,58 @@ using DistanceMatrix = vector<vector<int>>;
 // dynamic programming.
 
 
+class InputHandler {
+ public:
+  void ReadFromStdin();
 
+  vector<string> orienteering_map_;
+  Coordinate start_;
+  Coordinate goal_;
+  vector<Coordinate> checkpoints_;
+
+ private:
+  void RecordCoordinate(const int &row_index, const int &column_index);
+};
+
+void InputHandler::RecordCoordinate(const int &row_index,
+                                    const int &column_index) {
+  const char &symbol = orienteering_map_[row_index][column_index];
+  switch (symbol) {
+    case kStartSymbol: {
+      start_ = make_pair(row_index, column_index);
+      break;
+    }
+    case kGoalSymbol: {
+      goal_ = make_pair(row_index, column_index);
+      break;
+    }
+    case kCheckpointSymbol: {
+      checkpoints_.push_back(
+          make_pair(row_index, column_index));
+      break;
+    }
+  }
+}
+
+// Load orienteering map from standard input.
+void InputHandler::ReadFromStdin() {
+  // setup orienteering_map_.
+  int width = 0, height = 0;
+  cin >> width >> height;
+
+  for (int counter = 0; counter != height; ++counter) {
+    string line;
+    cin >> line;
+    orienteering_map_.push_back(line);
+  }
+
+  // find start, goal and checkpoints.
+  for (int row_index = 0; row_index != height; ++row_index) {
+    for (int column_index = 0; column_index != width; ++column_index) {
+      RecordCoordinate(row_index, column_index);
+    }
+  }
+}
 
 
 // Skeleton code for the examination.
@@ -44,6 +109,8 @@ class Orienteering {
 };
 
 void Orienteering::main() {
+  InputHandler input_handler;
+  input_handler.ReadFromStdin();
 }
 
 int main(int argc, char* argv[]) {
